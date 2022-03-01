cd73b57e012532809ce56dbd35089265238dfa62
Fran M <fr.me.jim@gmail.com>
2022-03-01 12:14:51 +0100

    added port in db connection

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,6 +5,7 @@ const { Sequelize } = sqlize;
 
 const DB_HOST: string | undefined = process.env.DB_HOST;
 const DB_NAME: string | undefined = process.env.DB_NAME;
+const DB_PORT: number | undefined = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined;
 const DB_USERNAME: string | undefined = process.env.DB_USERNAME;
 const DB_PASSWORD: string | undefined = process.env.DB_PASSWORD;
 
@@ -12,6 +13,7 @@ const logging: boolean = process.env.NODE_ENV === 'developement';
 
 const connection = new Sequelize({
     host: DB_HOST,
+    port: DB_PORT,
     database: DB_NAME,
     username: DB_USERNAME,
     password: DB_PASSWORD,
