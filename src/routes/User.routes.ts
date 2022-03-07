d014d3f7932798dd811bf2d5c6fab92663eb47dd
Fran M <fr.me.jim@gmail.com>
2022-03-07 14:51:27 +0100

    added user routes

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -10,3 +10,5 @@ router.post("/login", strategy.authenticate('local', { session: false }), UserCo
 router.get("/users", strategy.authenticate('jwt', { session: false }), AdminController.getAllUserInfo);
 router.get("/users/:id", strategy.authenticate('jwt', { session: false }), AdminController.getUserInfo);
 router.get("/users/me", strategy.authenticate('jwt', { session: false }), UserController.getUserProfileInfo);
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import { Router } from "express";
 
 import PassportConfig from "../auth/passport";
 
+import userRoutes from './User.routes';
+
 const router = Router();
 const passportConfig = new PassportConfig(passport);
 export const strategy = passportConfig.SetStrategy();
@@ -10,5 +12,7 @@ export const strategy = passportConfig.SetStrategy();
 router.get('/', () => {
     console.log("hello")
 });
+
+router.use('/', userRoutes);
 
 export default router;
