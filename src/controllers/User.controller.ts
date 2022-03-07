c8f7bc77d8c1ac945bd5ae632c31509020ed2ad3
Fran M <fr.me.jim@gmail.com>
2022-03-07 15:23:52 +0100

    fixing routes

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -70,7 +70,7 @@ class UserController {
      public static registerUser = async (req: Request, res: Response): Promise<Response> => {
         try {
             const user: UserCreate | undefined = req.body;
-            console.log(user)
+            console.log(req.body)
             if(!user) return res.sendStatus(400);
 
             const result = await User.create(
