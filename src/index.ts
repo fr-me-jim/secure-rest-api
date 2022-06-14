83f03b79187efc38f3f025dfbaf4b1968ea6a273
Francisco Mendoza Jimenez <francisco.mendoza.jimenez@estudiantat.upc.edu>
2022-06-14 11:51:19 +0200

    added morgan and multer imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // import path from 'path';
 import cors from 'cors';
+// import multer from "multer";
+import logger from "morgan";
 import dotenv from "dotenv";
 import express from "express";
 // import { fileURLToPath } from 'url';
@@ -16,17 +18,26 @@ import RouterAPI from "./routes/index";
 // database
 import connection from './models/index';
 
+// config
+// import storage from "./config/multer.storage";
+
+// utils
+// import { checkAllowedFiles } from './utils/helpers';
+
 const app = express();
 const routerAPI = new RouterAPI();
 const PORT: string | number = process.env.PORT || 9000;
 
 app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(passport.initialize());
 app.use('/api', routerAPI.InitializeRouter());
 app.use(express.static(`${__dirname}/public`));
 app.use(express.urlencoded({ extended: false }));
 
+// const upload = multer({ storage, fileFilter: (_req, file, callback) => checkAllowedFiles(file, callback) });
+console.log(__dirname)
 app.listen(PORT, async () => {
     try {
         await connection.authenticate();
