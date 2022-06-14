5c12f9a979632c857c3978643cc4983f133a29b4
Francisco Mendoza Jimenez <francisco.mendoza.jimenez@estudiantat.upc.edu>
2022-06-14 11:50:36 +0200

    added helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,18 @@
 // import fs from "fs";
+import path from 'path';
+import { FileFilterCallback } from 'multer';
 
 // const writeFileToFS = (file: ArrayBufferView): void => {
 //     // fs.writeFileSync(`${ __dirname }`, file)
 // };
+
+export const checkAllowedFiles = (file: Express.Multer.File, cb: FileFilterCallback): void => {
+    // allowed ext
+    const whitelist = /jpeg|jpg|png/;
+
+    // check extensions
+    const extension = path.extname(file.originalname).toLowerCase();
+    const isAllowed = whitelist.test(extension);
+
+    return cb(null, isAllowed);
+};
