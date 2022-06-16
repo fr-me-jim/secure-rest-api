7acacf0344368c4259fe0a5d334eb5bb9a9b654f
Francisco Mendoza Jimenez <francisco.mendoza.jimenez@estudiantat.upc.edu>
2022-06-16 17:47:08 +0200

    added Server class

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,65 @@
 import fs from "fs";
 import path from 'path';
-import cors from 'cors';
-import logger from "morgan";
+// import cors from 'cors';
+// import logger from "morgan";
 import dotenv from "dotenv";
-import express, { Request, Response, NextFunction } from "express";
+// import express, { Request, Response, NextFunction } from "express";
 // import { fileURLToPath } from 'url';
 
 // load environment variables
 dotenv.config();
 
 // passport
-import passport from 'passport';
+// import passport from 'passport';
 
 // routes
 import RouterAPI from "./routes/index";
 
 // database
 import connection from './models/index';
-import { ValidationError } from "sequelize";
+// import { ValidationError } from "sequelize";
+import APIServer from "./server";
 
 // utils
-// import { checkAllowedFiles } from './utils/helpers';
+// import  from './';
 
-const app = express();
+// const app = express();
 const routerAPI = new RouterAPI();
-const PORT: string | number = process.env.PORT || 9000;
+const PORT: number = (process.env.PORT && parseInt(process.env.PORT)) || 9000;
 const debugLevel: string = process.env.NODE_ENV === "production" ? "combined" : "dev"; 
-console.log(path.resolve('/var/log'), "server-access.log")
 const accessLogStream = fs.createWriteStream(path.join(path.resolve('/home/node/logs'), "server-access.log"), { flags: 'a+' });
 
-app.use(cors());
-app.use(express.json({}));
-app.use(passport.initialize());
-app.use('/api', routerAPI.InitializeRouter());
-app.use(express.static(`${__dirname}/public`));
-app.use(express.urlencoded({ extended: false }));
-app.use(logger(debugLevel, { stream: accessLogStream }));
+const server = new APIServer(PORT, debugLevel, connection, routerAPI, accessLogStream);
 
-app.use((error: unknown, _req: Request, res: Response, _next: NextFunction): Response =>{
-    console.log(error)
-    if (error instanceof ValidationError) return res.sendStatus(400);
 
-    return res.sendStatus(500);
-});
-console.log(path.join(path.resolve('./logs'), "server-access.log"))
-// const upload = multer({ storage, fileFilter: (_req, file, callback) => checkAllowedFiles(file, callback) });
-app.listen(PORT, async () => {
-    try {
-        await connection.authenticate();
-        console.log('Database connected.');
+server.listen();
 
-        await connection.sync({ force: true, });
-        console.log('Tables synced in Database.');
-    } catch (error: unknown) {
-        throw error;
-    }
+// app.use(cors());
+// app.use(express.json({}));
+// app.use(passport.initialize());
+// app.use('/api', routerAPI.InitializeRouter());
+// app.use(express.static(`${__dirname}/public`));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(logger(debugLevel, { stream: accessLogStream }));
+
+// app.use((error: unknown, _req: Request, res: Response, _next: NextFunction): Response =>{
+//     console.log(error)
+//     if (error instanceof ValidationError) return res.sendStatus(400);
+
+//     return res.sendStatus(500);
+// });
+// console.log(path.join(path.resolve('./logs'), "server-access.log"))
+// // const upload = multer({ storage, fileFilter: (_req, file, callback) => checkAllowedFiles(file, callback) });
+// app.listen(PORT, async () => {
+//     try {
+//         await connection.authenticate();
+//         console.log('Database connected.');
+
+//         await connection.sync({ force: true, });
+//         console.log('Tables synced in Database.');
+//     } catch (error: unknown) {
+//         throw error;
+//     }
     
-    console.log("Server running on port:", PORT);
-});
+//     console.log("Server running on port:", PORT);
+// });
---
+++ src/server/index.ts
@@ -0,0 +1,81 @@
+import express, 
+{ 
+    Router, 
+    Request, 
+    Response,
+    Application, 
+} from 'express';
+import cors from 'cors';
+import logger from "morgan";
+import passport from "passport";
+import { WriteStream } from "fs";
+import { Sequelize, ValidationError } from "sequelize";
+
+// router
+import RouterAPI from '../routes';
+
+export default class APIServer {
+    public readonly app: Application;
+
+    private port: number;
+    private router: Router
+    private debugLevel: string;
+    private connection: Sequelize
+    private accessLogStream: WriteStream;
+
+    constructor(port: number, debugLevel: string, connection: Sequelize, routerAPI: RouterAPI, logWriteStream: WriteStream) {
+        this.port = port;
+        this.app = express();
+        this.debugLevel = debugLevel;
+        this.connection = connection;
+        this.router = routerAPI.InitializeRouter();
+        this.accessLogStream = logWriteStream;
+
+        this.SetAssets();
+        this.SetRouter();
+        this.SetMiddlewares();
+    }
+
+    private readonly SetAssets = (): void => {
+      this.app.use(express.static(`${__dirname}/public`));  
+    };
+
+    private readonly SetRouter = (): void => {
+        this.app.use('/api', this.router);  
+    };
+
+    private readonly SetMiddlewares = (): void => {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(passport.initialize());
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(logger(this.debugLevel, { stream: this.accessLogStream }));
+
+        this.app.use(this.ExpressErrorHandler);  
+    };
+
+    private ExpressErrorHandler(error: unknown, _req: Request, res: Response): Response {
+        if (error instanceof ValidationError) return res.sendStatus(400);
+
+        return res.sendStatus(500);
+    };
+
+    /**
+     * Start Server on Listening Port 
+     */
+    public readonly listen = (): void => {
+        this.app.listen(this.port, async () => {
+            try {
+                await this.connection.authenticate();
+                console.log('Database connected.');
+        
+                await this.connection.sync({ force: true, });
+                console.log('Tables synced in Database.');
+            } catch (error: unknown) {
+                throw error;
+            }
+            
+            console.log("Server running on port:", this.port);
+        }); 
+    };
+};
