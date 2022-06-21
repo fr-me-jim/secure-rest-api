e7d139553961e3c3f54fd8f6cc904184d30805c2
Francisco Mendoza Jimenez <francisco.mendoza.jimenez@estudiantat.upc.edu>
2022-06-21 17:46:43 +0200

    added network

--- scripts/postgres-deploy.sh
+++ scripts/postgres-deploy.sh
@@ -5,5 +5,5 @@ cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem $DATA_DIR/server.crt
 cp /etc/letsencrypt/live/$DOMAIN/privkey.pem $DATA_DIR/server.key
 chmod 600 $DATA_DIR/server.crt
 chmod 600 $DATA_DIR/server.key
-chown 70 $DATA_DIR/server.crt
-chown 70 $DATA_DIR/server.key
+chown 70:70 $DATA_DIR/server.crt
+chown 70:70 $DATA_DIR/server.key
