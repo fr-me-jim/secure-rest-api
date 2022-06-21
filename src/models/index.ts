6a4d4d4341c510ad6a1b0fd6a037f25f7a9afca0
Francisco Mendoza Jimenez <francisco.mendoza.jimenez@estudiantat.upc.edu>
2022-06-21 19:21:06 +0200

    adding certs

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import sqlize from "sequelize";
 
 // logging
@@ -13,13 +12,6 @@ const DB_PORT: number | undefined = process.env.DB_PORT ? parseInt(process.env.D
 const DB_USERNAME: string | undefined = process.env.DB_USERNAME;
 const DB_PASSWORD: string | undefined = process.env.DB_PASSWORD;
 
-// const logging: boolean = process.env.NODE_ENV === 'developement';
-
-const serverCert  = fs.readFileSync("./.certs/server.crt");
-const serverCertKey  = fs.readFileSync("./.certs/server.key");
-console.log(serverCert)
-console.log(serverCertKey)
-
 const connection = new Sequelize({
     host: DB_HOST,
     port: DB_PORT,
@@ -27,14 +19,7 @@ const connection = new Sequelize({
     username: DB_USERNAME,
     password: DB_PASSWORD,
     dialect: 'postgres',
-    logging: msg => logger.debug(msg),
-    ssl: true,
-    dialectOptions: {
-        ssl: {
-            cert: serverCert,
-            key: serverCertKey
-        }
-    }
+    logging: msg => logger.debug(msg)
 });
 
 export default connection;
