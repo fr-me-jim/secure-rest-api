b3df633aae9a03aa7554f47777340ff581c05bf6
Francisco Mendoza Jimenez <francisco.mendoza.jimenez@estudiantat.upc.edu>
2022-06-21 17:02:00 +0200

    added network

--- scripts/postgres-deploy.sh
+++ scripts/postgres-deploy.sh
@@ -4,4 +4,5 @@ DOMAIN=tfm.jediupc.com
 DATA_DIR=/var/lib/postgresql/data
 cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem $DATA_DIR/server.crt
 cp /etc/letsencrypt/live/$DOMAIN/privkey.pem $DATA_DIR/server.key
+cd /home/tfm-server/tfm-backend/ && docker-compose restart
 # chown postgres:postgres $DATA_DIR/server.crt $DATA_DIR/server.key
