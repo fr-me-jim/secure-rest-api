0a0ec2e6d7bf656078cea08a49966f03d31bf5cd
Francisco Mendoza Jimenez <francisco.mendoza.jimenez@estudiantat.upc.edu>
2022-07-05 11:21:11 +0200

    added start app commands

--- scripts/postgres-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DOMAIN=tfm.jediupc.com
-DATA_DIR=/var/lib/postgresql/data
-cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem $DATA_DIR/server.crt
-cp /etc/letsencrypt/live/$DOMAIN/privkey.pem $DATA_DIR/server.key
-chmod 600 $DATA_DIR/server.crt
-chmod 600 $DATA_DIR/server.key
-chown 70:70 $DATA_DIR/server.crt
-chown 70:70 $DATA_DIR/server.key
